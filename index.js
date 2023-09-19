485c75094bca88eeed16cb017adca6aba2763fd2
Nikita Gutman <gut@d-element.ru>
2023-09-19 10:41:06 +0500

    fix func logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const arrayData = ['1.5', '3', '6', '1.5'];
 
 if (arrayData.length < 50000) {
-    const percentages = arrayData.map(item => (item * 100).toFixed(3));
-    console.log(percentages);
+    const percentages = arrayData.map(item => (parseFloat(item) / arrayData.reduce((acc, val) => acc + parseFloat(val), 0)) * 100);
+    const formattedPercentages = percentages.map(percentage => percentage.toFixed(3));
+    console.log(formattedPercentages);
 }
